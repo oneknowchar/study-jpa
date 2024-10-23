52aff3a4820491892606d3ca3dc1432705bade6a
oneknowchar <oneknowchar@gmail.com>
2024-10-23 22:55:25 +0900

    JPA 페이징처리

--- ex1-hello-jpa-start/src/main/java/hellojpa/Main.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Main.java
@@ -1,13 +1,12 @@
 package hellojpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import hellojpa.entity.Member;
-import hellojpa.entity.Team;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.NoResultException;
 import jakarta.persistence.Persistence;
 
 public class Main {
@@ -19,43 +18,50 @@ public class Main {
 
 		
 		try {
-			Member member1 = new Member();
-			member1.setUserName("user01");
+			List<Member> newMembers = makeMember(100, em, tx);	//신규 데이터 생성
 			
-			Member member2 = new Member();
-			member2.setUserName("user02");
-			
-			em.persist(member1);
-			em.persist(member2);
-			
-			em.flush();
+			//영속성 초기화
+			//em.flush();	
 			em.clear();
 			
-			//JPQL_ example_1
-			List<Member> result1 = em
-									.createQuery("select m From Member m where m.userName like 'user%'", Member.class)
-									.getResultList();
+			//전체 조회
+			//List<Member> saveMembers = em.createQuery("select m from Member as m order by m.id", Member.class).getResultList();
 			
-			//JPQL_ example_2
-			List<Member> result2 = em
-									.createQuery("select m From Member m where m.id > 10", Member.class)
-									.getResultList();
+			//페이징 조회
+			List<Member> saveMembers = em.createQuery("select m from Member as m order by m.id desc", Member.class)
+					.setFirstResult(0)
+					.setMaxResults(10)
+					.getResultList();
 			
-			Member result3 = null;
-			try {
-				//결과가 없으면 Exception
-				result3 = em.createQuery("select m From Member m where m.userName = :userName", Member.class)
-						.setParameter("userName", "user007")
-						.getSingleResult();
-			}catch (NoResultException e) {
-				e.printStackTrace();
+			System.out.println(saveMembers.size());
+			for(Member findMember : saveMembers) {
+				System.out.println("findMember Name = " + findMember.getUserName());
 			}
 			
-			System.out.println(result1);
-			System.out.println(result2);
-			System.out.println(result3);
 			
-			tx.commit();
+		/* select
+	        m 
+	    from
+	        Member as m 
+	    order by
+	        m.id desc  select
+	            m1_0.id,
+	            m1_0.age,
+	            m1_0.createdBy,
+	            m1_0.createdDate,
+	            m1_0.lastModifiedBy,
+	            m1_0.lastModifiedDate,
+	            m1_0.team_id,
+	            m1_0.userName 
+	        from
+	            Member m1_0 
+	        order by
+	            m1_0.id desc 
+	        offset
+	            ? rows 
+	        fetch
+	            first ? rows only
+			*/
 		} catch (Exception e) {
 			e.printStackTrace();
 			tx.rollback();
@@ -63,5 +69,34 @@ public class Main {
 			em.close();
 		}
 		emf.close();
+	}
+	
+	/**
+	 * 페이징 연습 더미 데이터 생성
+	 * 영속성 EntityMananger, DB insert
+	 * @param num 디비에 저장하고 싶은 회원의 수
+	 * @param em 영속성 관리를 위한 엔티티 메니저
+	 * @return result<boolean>
+	 */
+	public static List<Member> makeMember(int num, EntityManager em, EntityTransaction tx) {
+		List<Member> members = new ArrayList<>();
+		
+		for(int i = 0; i < num; i++) {
+			Member member = new Member();
+			
+			if(i < 10) {
+				member.setUserName("user0" + i);
+			}else {
+				member.setUserName("user" + i);
+			}
+			
+			//영속성 저장
+			members.add(member);
+			em.persist(member);
+		}
+		
+		tx.commit();	//커밋 //DB 저장
+		
+		return members;
 	}
 }
